60151d28dcaed1c68646a64f4ce8e45d74b26464
Zukhrab <zukhrab.asakhanov@bk.ru>
2023-07-20 14:18:12 +0300

    in process(added icons

--- main.py
+++ main.py
@@ -1,91 +1,70 @@
 import os
 import PyPDF2 as pdf2
-from TTS.api import TTS
+import tkinter
+import time
+ 
 
+Window_Width=400
 
-file_name = "Baron_En.pdf"
-file_path = os.getcwd()
+Window_Height=1000
 
-pdfFileObj = open(file_name, 'rb')
-pdfReader = pdf2.PdfReader(pdfFileObj)
+Ball_Start_XPosition = 50
 
-#print(pdfReader.numPages)
-print(len(pdfReader.pages))
+Ball_Start_YPosition = 50
 
-pageObj = pdfReader.pages[18]
-print(pageObj.extract_text())
+Ball_Radius = 30
 
-# Running a multi-speaker and multi-lingual model
+Ball_min_movement = 5
 
-# # List available 🐸TTS models and choose the first one
-# model_name = TTS.list_models()[0]
-# # Init TTS
-# tts = TTS(model_name)
+Refresh_Sec = 0.01
+ 
 
-# # Run TTS
+def create_animation_window():
+  Window = tkinter.Tk()
+  Window.title("Python Guides")
 
-# # ❗ Since this model is multi-speaker and multi-lingual, we must set the target speaker and the language
-# # Text to speech with a numpy output
-# wav = tts.tts("This is a test! This is also a test!!", speaker=tts.speakers[0], language=tts.languages[0])
-# # Text to speech to a file
-# tts.tts_to_file(text = pageObj.extract_text(), speaker=tts.speakers[0], language=tts.languages[0], file_path="output.wav")
+  Window.geometry(f'{Window_Width}x{Window_Height}')
+  return Window
+ 
 
-# # Running a single speaker model
+def create_animation_canvas(Window):
+  canvas = tkinter.Canvas(Window)
+  canvas.configure(bg="Blue")
+  canvas.pack(fill="both", expand=True)
+  return canvas
+ 
 
-# # Init TTS with the target model name
-# tts = TTS(model_name="tts_models/de/thorsten/tacotron2-DDC", progress_bar=False, gpu=False)
-# # Run TTS
-# tts.tts_to_file(text="Ich bin eine Testnachricht.", file_path=OUTPUT_PATH)
+def animate_ball(Window, canvas,xinc,yinc):
+  ball = canvas.create_oval(Ball_Start_XPosition-Ball_Radius,
+            Ball_Start_YPosition-Ball_Radius,
+            Ball_Start_XPosition+Ball_Radius,
+            Ball_Start_YPosition+Ball_Radius,
+            fill="Red", outline="Black", width=4)
+  while True:
+    canvas.move(ball,xinc,yinc)
+    Window.update()
+    time.sleep(Refresh_Sec)
+    ball_pos = canvas.coords(ball)
+    # unpack array to variables
+    al,bl,ar,br = ball_pos
+    if al < abs(xinc) or ar > Window_Width-abs(xinc):
+      xinc = -xinc
+    if bl < abs(yinc) or br > Window_Height-abs(yinc):
+      yinc = -yinc
+ 
 
-# # Example voice cloning with YourTTS in English, French and Portuguese
+Animation_Window = create_animation_window()
+Animation_canvas = create_animation_canvas(Animation_Window)
+animate_ball(Animation_Window,Animation_canvas, Ball_min_movement, Ball_min_movement)
 
-# tts = TTS(model_name="tts_models/multilingual/multi-dataset/your_tts", progress_bar=False, gpu=True)
-# tts.tts_to_file("This is voice cloning.", speaker_wav="my/cloning/audio.wav", language="en", file_path="output.wav")
-# tts.tts_to_file("C'est le clonage de la voix.", speaker_wav="my/cloning/audio.wav", language="fr-fr", file_path="output.wav")
-# tts.tts_to_file("Isso é clonagem de voz.", speaker_wav="my/cloning/audio.wav", language="pt-br", file_path="output.wav")
+# file_name = "Baron_En.pdf"
+# file_path = os.getcwd()
 
+# pdfFileObj = open(file_name, 'rb')
+# pdfReader = pdf2.PdfReader(pdfFileObj)
 
-# # Example voice conversion converting speaker of the `source_wav` to the speaker of the `target_wav`
+# #print(pdfReader.numPages)
+# print(len(pdfReader.pages))
 
-# tts = TTS(model_name="voice_conversion_models/multilingual/vctk/freevc24", progress_bar=False, gpu=True)
-# tts.voice_conversion_to_file(source_wav="my/source.wav", target_wav="my/target.wav", file_path="output.wav")
-
-# # Example voice cloning by a single speaker TTS model combining with the voice conversion model. This way, you can
-# # clone voices by using any model in 🐸TTS.
-
-# tts = TTS("tts_models/de/thorsten/tacotron2-DDC")
-# tts.tts_with_vc_to_file(
-#     "Wie sage ich auf Italienisch, dass ich dich liebe?",
-#     speaker_wav="target/speaker.wav",
-#     file_path="output.wav"
-# )
-
-# # Example text to speech using [🐸Coqui Studio](https://coqui.ai) models.
-
-# # You can use all of your available speakers in the studio.
-# # [🐸Coqui Studio](https://coqui.ai) API token is required. You can get it from the [account page](https://coqui.ai/account).
-# # You should set the `COQUI_STUDIO_TOKEN` environment variable to use the API token.
-
-# # If you have a valid API token set you will see the studio speakers as separate models in the list.
-# # The name format is coqui_studio/en/<studio_speaker_name>/coqui_studio
-# models = TTS().list_models()
-# # Init TTS with the target studio speaker
-# tts = TTS(model_name="coqui_studio/en/Torcull Diarmuid/coqui_studio", progress_bar=False, gpu=False)
-# # Run TTS
-# tts.tts_to_file(text="This is a test.", file_path=OUTPUT_PATH)
-# # Run TTS with emotion and speed control
-# tts.tts_to_file(text="This is a test.", file_path=OUTPUT_PATH, emotion="Happy", speed=1.5)
-
-
-# #Example text to speech using **Fairseq models in ~1100 languages** 🤯.
-
-# #For these models use the following name format: `tts_models/<lang-iso_code>/fairseq/vits`.
-# #You can find the list of language ISO codes [here](https://dl.fbaipublicfiles.com/mms/tts/all-tts-languages.html) and learn about the Fairseq models [here](https://github.com/facebookresearch/fairseq/tree/main/examples/mms).
-
-# # TTS with on the fly voice conversion
-# api = TTS("tts_models/deu/fairseq/vits")
-# api.tts_with_vc_to_file(
-#     "Wie sage ich auf Italienisch, dass ich dich liebe?",
-#     speaker_wav="target/speaker.wav",
-#     file_path="output.wav"
-# )
+# pageObj = pdfReader.pages[18]
+# print(pageObj.extract_text())
